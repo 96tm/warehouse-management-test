3fda4d4a4575fe4d8e90d21418fed32a9e4014ac
vism <khabcity@gmail.com>
2020-02-02 19:23:54 +1000

    Карточка покупателя теперь показывает список сделанных заказов (дата, статус) с возможностью изменения статуса

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
 
 from .models import Supplier, Customer, Stock, Category
+from .models import Shipment
 
-# Register your models here.
+# функция gettext с псевдонимом _ применяется к строками на английском для последующего перевода
+
+
+class CustomerAdmin(admin.ModelAdmin):
+
+    class ShipmentInline(admin.StackedInline):
+        model = Shipment
+        readonly_fields = ('date',)
+        fields = ['date', 'status']
+        list_display = ['date', 'status']
+        can_delete = False
+        min_num = 0
+        max_num = 0
+        verbose_name = _("Shipment")
+        verbose_name_plural = _("Shipments")
+
+    inlines = [ShipmentInline, ]
+    fieldsets = (
+        (_('CUSTOMER DATA'),
+         {'fields': ('full_name', 'phone_number',
+                     'email', 'contact_info')}),
+    )
+
 
 admin.site.register(Supplier)
-admin.site.register(Customer)
+admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Stock)
 admin.site.register(Category)
