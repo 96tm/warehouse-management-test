7d4cd90a3ec91f5f46c2eca144a4523342642b38
Eastrong <tmp100500@mail.ru>
2020-02-04 17:42:49 +1000

    renamed views

--- warehouse/forms.py
+++ warehouse/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import Cargo, CargoDetails
 
 
-class NeworderForm(forms.ModelForm):
+class CargoNewForm(forms.ModelForm):
 
     class Meta:
         model = Cargo
         fields = ('supplier',)
 
 
-class OrderdetailsForm(forms.ModelForm):
+class CargoFillForm(forms.ModelForm):
 
     class Meta:
         model = CargoDetails
