0b5e0f5d6dcf16823bcddc3c68e743d2794bdfcc
angel <angel_hab@mail.ru>
2020-02-07 07:19:07 +1000

    Начал делать страницу заказа

--- warehouse/forms.py
+++ warehouse/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class OrderForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(label='Your name', max_length=100)
 
     def send_email(self):
         pass
--- warehouse/views.py
+++ warehouse/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import OrderForm
 from django.views.generic.edit import FormView
 from django.views.generic import View
@@ -10,21 +10,20 @@ def index(request):
 
 # страница покупателя
 class OrderView(FormView):
-    template_name = "warehouse//order.html"
+    template_name = "warehouse/order.html"
     form_class = OrderForm
     success_url = "order_successful"
 
-    def form_valid(self, form):
-        form.send_email()
-        return super().form_valid(form)
+    def post(self, request):
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('order_successful')
 
 
 class OrderSuccessfulView(View):
     def get(self, request):
-        context = {
-            "name": "123",
-        }
-        return render(request, 'warehouse/order_successful.html', context)
+        context = request
+        return render(request, 'warehouse/order_successful.html')
 
 
 def supplier(request):
