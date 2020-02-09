6f58ec4c3f227912ccfad527202ad3c325a75781
angel <angel_hab@mail.ru>
2020-02-10 01:03:27 +1000

    Начал делать страницу заказа

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -6,12 +6,11 @@ app_name = 'warehouse'
 
 urlpatterns = [path('', views.index, name='index'),
                path('supplier', views.supplier, name='supplier'),
-               path('customer', views.customer, name='customer'),
                path('shipment_confirmation',
                     views.ShipmentConfirmation.as_view(),
                     name='shipment_confirmation'),
                path('shipment_success', views.shipment_success,
-                    name='shipment_success')
-               path('customer', views.OrderView.as_view(), name='order'),
+                    name='shipment_success'),
+               path('order', views.OrderView.as_view(), name='order'),
                path('order_successful', views.OrderSuccessfulView.as_view(), name='order_successful'),
                ]
