1b3c47fcc1a16b3c2468355d635d4185c8b4a552
Evilko <Evilko@users.noreply.github.com>
2020-02-11 16:00:12 +1000

    Продолжаем работать над страницей заказа.

--- warehouse/forms.py
+++ warehouse/forms.py
@@ -9,7 +9,7 @@ class OrderForm(forms.Form):
         customers = kwargs.get('initial')['name']
         customers_list = [(k, v) for k,v in customers]
         items = kwargs.get('initial')['items']
-        items_list = [('text', t) for t in items]
+        items_list = [(k, v) for k, v in items]
         super(OrderForm, self).__init__(*args, **kwargs)
         self.fields['name'].choices = customers_list
         self.fields['items'].choices = items_list
--- warehouse/views.py
+++ warehouse/views.py
@@ -15,18 +15,6 @@ def index(request):
     return render(request, 'warehouse/index.html')
 
 
-# страница покупателя
-# def order_view(request):
-#     if request.method == 'POST':
-#         form = OrderForm(request.POST)
-#         if form.is_valid():
-#             return HttpResponseRedirect('order_successful')
-#
-#     else:
-#         form = OrderForm()
-#
-#     return render(request, 'warehouse/order.html', {'form': form})
-
 class OrderView(FormView):
     template_name = "warehouse/order.html"
     form_class = OrderForm
@@ -36,21 +24,20 @@ class OrderView(FormView):
         # return super(OrderView, self).form_valid(form)
         data = form.cleaned_data
         customer = Customer.objects.get(id=data['name'])
-        print(data)
-        print(customer)
+        product = Stock.objects.get(article=data['items'])
         context = {
             'customer_name': customer.full_name,
-            'customer_id': customer.id
+            'customer_id': customer.id,
+            'product_name': product.name,
         }
         return render(self.request, 'warehouse/order_successful.html', context)
 
     def get_initial(self):
         initial = super(OrderView, self).get_initial()
         customers_list = Customer.objects.all().order_by('full_name').values_list('id', 'full_name')
-        items_list = Stock.objects.all().values_list()
+        items_list = Stock.objects.all().values_list('article', 'name')
         initial.update({'name': customers_list,
-                        'items': ['1', '2', '3']})
-        print('1', initial)
+                        'items': items_list})
         return initial
 
 
